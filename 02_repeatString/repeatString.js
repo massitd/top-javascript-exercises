3bd8abbbf09d7cacfa2f5f9e25787feaf33c6716
massitd <82796634+massitd@users.noreply.github.com>
2024-08-01 20:36:30 -0400

    Add solution to 01, 02.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let loopString = "";
+    
+    if (num < 0) {
+        loopString = "ERROR";
+    }
 
-};
+    for (let i = 0; i < num; i++) {
+        loopString = loopString + string;
+    }
 
+    return loopString;
+}
 // Do not edit below this line
 module.exports = repeatString;
