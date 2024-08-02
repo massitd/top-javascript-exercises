0dbcd55b2d6644d5fc6d28b80f477cccd897417c
massitd <82796634+massitd@users.noreply.github.com>
2024-08-02 14:12:14 -0400

    Add solution for 03.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strArr = str.split('');
+    strArr = strArr.reverse();
+    reversed = strArr.join('');
+    console.log(reversed);
+    return reversed;
 };
 
 // Do not edit below this line
