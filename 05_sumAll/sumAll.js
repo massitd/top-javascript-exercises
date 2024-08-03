e3e9c7e414d228a07de39256cb2bb6312739d66e
massitd <82796634+massitd@users.noreply.github.com>
2024-08-03 17:06:54 -0400

    Add solution to exercise 06.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
 
+    if (Number.isInteger(a) == false) {
+        return "ERROR";
+    }
+    if (Number.isInteger(b) == false) {
+        return "ERROR";
+    }
+    if (a < 0) {
+        return "ERROR";
+    }
+
+    if (a < b) {
+
+        for (a = a; a <= b; a++) {
+            sum = sum + a;
+            console.log(sum);
+        };
+    } else {
+        for (b = b; b <= a; b++) {
+            sum = sum + b;
+            console.log(sum);
+        }
+    }
+    console.log(sum);
+    return sum;
 };
 
 // Do not edit below this line
