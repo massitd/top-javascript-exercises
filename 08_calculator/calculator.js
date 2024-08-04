3d290dce48def2ec3d0fb6e8ae7875b9c5fa67ce
massitd <82796634+massitd@users.noreply.github.com>
2024-08-03 21:25:49 -0400

    Add solution to 08.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const add = function(a, b) {
+  return a + b;
 
 };
 
-const power = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const factorial = function() {
-	
+const sum = function(arr) {
+  let sum = 0
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
+};
+
+const multiply = function(arr) {
+  let mult = 1
+  for (let i = 0; i < arr.length; i++) {
+    mult = mult * arr[i];
+  }
+  return mult;
+};
+
+const power = function(a, b) {
+	return a**b;
+};
+
+const factorial = function(a) {
+	let fac = 1
+  for (let i = 1; i <= a; i++) {
+    fac = fac * i;
+  }
+  return fac;
 };
 
 // Do not edit below this line
